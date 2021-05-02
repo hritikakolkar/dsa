627754547404fe3843b8696defc87739783a41aa
Hritik Akolkar <48088844+HRITIKAKOLKAR@users.noreply.github.com>
2021-05-02 14:17:36 +0530

    Add files via upload

---
+++ algorithm/sorting/insertion-sort.py
@@ -0,0 +1,6 @@
+#Taking user input list to sort the list using Insertion sort.
+userIpLst=list(map(int,input().split()))
+#Let's get started
+for index in range(1,len(userIpLst)):
+    for revindex in range(index):
+        
