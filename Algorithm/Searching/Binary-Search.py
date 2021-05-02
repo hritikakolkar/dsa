45271c0d57cf5d11b402dd14b284b7c2afa0d181
Hritik Akolkar <48088844+HRITIKAKOLKAR@users.noreply.github.com>
2021-05-02 14:27:07 +0530

    Add files via upload

---
+++ Algorithm/Searching/Binary-Search.py
@@ -0,0 +1,9 @@
+#taking input of values in array and searching for given value in array using binary search technique:
+userIp=list(map(int,input().split()))
+value=int(input())
+def binary_search(lst,val):
+    if len(lst)>1:
+        mid=(start+end)//2
+        while start<end:
+            if mid
+    return 0
---
+++ Algorithm/Sorting/Bubble-Sort.py
@@ -0,0 +1,10 @@
+#Taking input from user to sort using bubble. 
+user_lst=list(map(int,input().split()))
+#Let's get started.
+#length of user inputed array.
+len_user_lst=len(user_lst)
+for index in range( len_user_lst ):
+    for indexless in range( len_user_lst - index - 1 ):
+        if user_lst[indexless]>user_lst[indexless+1]:
+            user_lst[indexless],user_lst[indexless+1]=user_lst[indexless+1],user_lst[indexless]
+print(user_lst)
