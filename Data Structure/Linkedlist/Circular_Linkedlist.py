6bc1b8e1204bf547f9b212c26b3dfe559e7bad4b
HRITIKAKOLKAR <48088844+HRITIKAKOLKAR@users.noreply.github.com>
2021-05-17 12:47:09 +0530

    Update Circular_Linkedlist.py

---
+++ Data Structure/Linkedlist/Circular_Linkedlist.py
@@ -0,0 +1,62 @@
+"""
+This program is written by Hritik Akolkar
+Follow me on Linkedin and Github
+"""
+class Node :
+    def __init__(self, data :object, next = None) -> None:
+        self.data = data
+        self.next = next
+class Circular_Linkedlist :
+    def __init__(self):
+        self.head = None
+        self.length = 0
+
+    def append(self, data : object) -> None :
+        if self.head == None :
+            self.head = Node(data = data, next = None)
+            self.head.next = self.head
+        else :
+            current_Node = self.head
+            while current_Node.next != self.head :
+                current_Node = current_Node.next
+            current_Node.next = Node(data = data, next = self.head)
+
+    def prepend(self, data : object) -> None :
+        if self.head == None :
+            self.head = Node(data = data, next = None)
+            self.head.next = self.head    
+        else :
+            current_Node = self.head
+            while current_Node.next != None :
+                current_Node = current_Node.next
+            current_Node.next = Node(data = data,next = self.head)
+            self.head = current_Node.next
+        
+    def insert(self, data : object, position : int) -> None :
+        if position <= 0 :
+            self.prepend()
+        elif position >= self.length - 1:
+            self.append()
+        else:
+            current_Node = self.head
+            index = 1
+            while index < position :
+                current_Node = current_Node.next
+                index += 1 
+            current_Node.next = Node(data = data, next = current_Node.next)
+    
+    def removeFirst(self) -> None :
+        if self.head == None :
+            print("Circular Linked list is empty")
+        else :
+            current_Node = self.head
+            while current_Node.next != self.head :
+                current_Node = current_Node.next
+            self.head = self.head.next
+            current_Node.next = self.head
+
+    def removeLast(self) -> None :
+        pass
+    def remove(self, position : int) -> None :
+        pass
+    
