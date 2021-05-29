1cb23dcccdc7976bdbf3248bce8f286607fbf77a
HRITIKAKOLKAR <48088844+HRITIKAKOLKAR@users.noreply.github.com>
2021-05-29 12:00:12 +0530

    Changed name of Directories

--- Algorithm/Sorting/Insertion_Sort.py
+++
@@ -1,6 +0,0 @@
-#Taking user input list to sort the list using Insertion sort.
-userIpLst=list(map(int,input().split()))
-#Let's get started
-for index in range(1,len(userIpLst)):
-    for revindex in range(index):
-        pass
