1b7bfd717c743420c3627db5a2f1c19e01216106
HRITIKAKOLKAR <48088844+HRITIKAKOLKAR@users.noreply.github.com>
2021-06-01 22:26:36 +0530

    Update Singly_Linkedlist.py

--- Data Structures/Linkedlist/Singly_Linkedlist.py
+++ Data Structures/Linkedlist/Singly_Linkedlist.py
@@ -14,21 +14,24 @@ class Node:
         self.data = data
         self.next = next
 
-class Singly_Linkedlist:
+class singlyLinkedlist:
     """
     Operations on a LinkedList
     1) Travesing List
     2) Inserting an item in the list
     3) Deleting an item from the list
+    Input : None
     """
+    
     def __init__(self, head = Node()) -> None:
         self.head = head
         self.length = 0
     
     def __len__(self) -> int :
+        if self.length <=0 : self.length = 0 
         return self.length
         
-    def printlist(self) -> None :
+    def printList(self) -> None :
         current_Node = self.head
         while current_Node != None :
             print(str(current_Node.data),end = "->")
@@ -37,7 +40,7 @@ class Singly_Linkedlist:
     
     def prepend(self, data : object) -> None :
         if self.head == None :
-                self.head = Node(data = data, next = None)
+            self.head = Node(data = data, next = None)
         else :
             self.head = Node(data = data, next = self.head)
         self.length += 1
@@ -53,7 +56,7 @@ class Singly_Linkedlist:
             self.length += 1
 
     def insert(self, position : int, data : object ) -> None :
-        if position == 0 :
+        if position <= 0 :
             self.append(data)
         elif position >= self.length :
             self.prepend(data)
@@ -66,21 +69,18 @@ class Singly_Linkedlist:
             current_Node.next = Node(data= data, next = current_Node.next)
             self.length += 1
     
-    def remove_First(self) -> None :
+    def removeFirst(self) -> None :
         self.head = self.head.next
         self.length -= 1
-    def remove_Last(self) -> None :
-        current_Node = self.head
-        while current_Node.next.next != None :
-            current_Node = current_Node.next
-        current_Node.next = None
-        self.length -= 1
         
+    def removeLast(self) -> None :
+        pass
+    
     def remove(self, position : int) -> None :
-        if position == 0 :
-            self.remove_First()
+        if position <= 0 :
+            self.removeFirst()
         elif position >= self.length :
-            self.remove_Last()
+            self.removeLast()
         else :
             current_Node = self.head
             index=1
@@ -88,3 +88,15 @@ class Singly_Linkedlist:
                 current_Node = current_Node.next
                 index += 1
             current_Node.next = current_Node.next.next
+            self.length -= 1
+if __name__ == '__main__':
+    #tutorial
+    sllist = singlyLinkedlist()
+    for number in range(91,92) :
+        sllist.append(number)
+    sllist.removeFirst()
+    sllist.removeLast()
+    sllist.remove(position = 4)
+    print(len(sllist))
+    sllist.printList()    
+    
